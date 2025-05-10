12b3abcbc766b54dd0abc03e196001c5c8af5b74
PromiseBoloka <promlyboloka@gmail.com>
2025-05-10 21:49:58 +0200

    MazalaEJBModule

---
+++ MazalaEJBModule/ejb/bl/EmployeesFacadeLocal.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.ejb.bl;
+
+import java.util.List;
+import javax.ejb.Local;
+import za.ac.tut.entities.Employees;
+
+/**
+ *
+ * @author user
+ */
+@Local
+public interface EmployeesFacadeLocal {
+
+    void create(Employees employees);
+
+    void edit(Employees employees);
+
+    void remove(Employees employees);
+
+    Employees find(Object id);
+
+    List<Employees> findAll();
+
+    List<Employees> findRange(int[] range);
+
+    int count();
+    
+    List<Employees> longestService();
+    Long cntFemaleEmployees();
+    Long cntMaleEmployees();
+    List <Employees> youngestEmployee();
+    Double averageAge();
+    
+}
